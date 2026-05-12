489b48416ec99f285fe85c7da96f6b1007e95165
JamesASkibidi <0243@balcarras.gloucs.sch.uk>
2026-05-12 12:17:23 +0000

    Added signup page and signup function

--- main.py
+++ main.py
@@ -1,10 +1,22 @@
-from flask import Flask
+from flask import Flask, render_template, request
 
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "hellow World"
+    return render_template('signup.html')
+
+
+
+@app.route("/signup", methods=["POST"])
+def signup():
+
+    form_data = request.form
+    email = form_data.get("email")
+    username = form_data.get("username")
+    password = form_data.get("password")
+    print(email, username , password)
+    return "Account Created!"
 
 app.run(debug=True)
